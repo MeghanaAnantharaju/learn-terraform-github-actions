905a184c579251f1ccf9fd1e79102c4f182a3613
MeghanaAnantharaju <106523671+MeghanaAnantharaju@users.noreply.github.com>
2023-02-02 13:15:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "random_pet" "sg" {}
 
 
 resource "aws_instance" "web" {
-  ami                    = ami-0aa7d40eeae50c9a9
+  ami                    = "ami-0aa7d40eeae50c9a9"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 

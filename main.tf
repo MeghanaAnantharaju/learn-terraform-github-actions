a7d8a93f5fedb597dddb225a967859fbec367aff
MeghanaAnantharaju <meghana_anantharaju@epam.com>
2023-02-06 17:33:11 +0530

    Point backend crct tfc org and workspace

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "random_pet" "sg" {}
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0aa7d40eeae50c9a9"
+  ami                    = "ami-00874d747dde814fa"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 

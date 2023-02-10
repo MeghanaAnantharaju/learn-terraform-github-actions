6841bcb488f3880366d6615cfac48815060028d8
MeghanaAnantharaju <106523671+MeghanaAnantharaju@users.noreply.github.com>
2023-02-10 10:54:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "4.52.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "3.4.3"
     }
   }
   required_version = ">= 1.1.0"
@@ -26,9 +26,24 @@ provider "aws" {
 
 resource "random_pet" "sg" {}
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["312660557575"] # Canonical
+}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0aa7d40eeae50c9a9"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
